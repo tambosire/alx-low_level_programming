e9ca19248734b5365511a86be11c63131e7e8895
tambosire <mrghazali@st.ug.edu.gh>
2023-02-17 16:04:18 -0800

    variales

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <studio>
+/**
+ * main - main block
+ * Description: print the alphabet in lower case
+ * and the in uppercase,followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+	
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <='Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}	
